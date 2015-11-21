5abb6455ce1e86e4eef75905bbb405043e2acc20
Donnell Jenkins <donnell_jenkins@hotmail.com>
2015-11-20 23:27:01 -0600

    refactor(column-template): Changed to custom element
    Changed the grid-column-template from a custom attribute to a custom
    element.

--- src/grid/grid-column-template.js
+++ src/grid/grid-column-template.js
@@ -1,69 +1,30 @@
-﻿import startCase from "lodash/string/startCase";
-import { inject, customAttribute } from 'aurelia-framework';
+﻿import { Grid } from "./grid";
+import { ColumnUtility } from "./grid-column-utility";
+import { bindable, containerless, inject } from "aurelia-framework";
+import { ObserverLocator } from 'aurelia-binding';
 
-const propertyAttributeName = 'property';
-const headingAttributeName = 'heading';
-const filterableAttributeName = 'filterable';
-const sortableAttributeName = 'sortable';
-
-@customAttribute('grid-column-template')
-@inject(Element)
+@containerless
+@inject(Grid, ColumnUtility, Element, ObserverLocator)
 export class GridColumnTemplate {
+    @bindable heading;
+    @bindable filterable;
+    @bindable property;
+    @bindable sortable;
 
-    constructor(element) {
-        this.column = {};
+    constructor(grid, utility, element, observerLocator) {
         this.element = element;
+        this.grid = grid;
+        this.observerLocator = observerLocator;
+        this.row = {};
+        this.utility = utility;
     }
 
-    bind(bindingContext) {
-        if (bindingContext.addColumn) {
-            this.fillColumnInformationFromAttributes();
-            bindingContext.addColumn(this.column);
+    bind(bindingContext){
+        if(bindingContext === this.grid) {
+            this.utility.registerWithGrid(this.grid, this);
         }
-    }
-
-    fillColumnInformationFromAttributes() {
-        this.getColumnProperty();
-        this.getColumnHeading();
-        this.getColumnFilter();
-        this.getColumnSort();
-    }
-
-    getColumnProperty() {
-        let property = this.element.attributes.getNamedItem(propertyAttributeName);
-        if (property) {
-            this.column[propertyAttributeName] = property.value;
-        }
-    }
-
-    getColumnHeading() {
-        let attribute = this.element.attributes.getNamedItem(headingAttributeName);
-        if (attribute) {
-            this.column[headingAttributeName] = attribute.value;
-        } else if(this.column[propertyAttributeName]) {
-            this.column[headingAttributeName] = startCase(this.column[propertyAttributeName]);
-        }
-    }
-
-    getColumnFilter() {
-        let attribute = this.element.attributes.getNamedItem(filterableAttributeName);
-        if (attribute) {
-            this.column[filterableAttributeName] = true;
-            this.column.filter = {
-                property: attribute.value || this.column[propertyAttributeName],
-                value: undefined
-            }
-        }
-    }
-
-    getColumnSort() {
-        let attribute = this.element.attributes.getNamedItem(sortableAttributeName);
-        if (attribute) {
-            this.column[sortableAttributeName] = true;
-            this.column.sort = {
-                property: attribute.value || this.column[propertyAttributeName],
-                value: undefined
-            }
+        else {
+            this.utility.bindToRow(bindingContext, this);
         }
     }
 }
